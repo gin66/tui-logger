5a47184eef4487d09042649f95e04fb43c4a32f2
Jochen Kiemes <jochen@kiemes.de>
2018-05-25 22:03:50 +0800

    use a normal HashMap in hot logger code instead of an array

--- src/lib.rs
+++ src/lib.rs
@@ -8,26 +8,29 @@
 //!
 //! ## Documentation
 //! 
-//! [Documentation](https://docs.rs/tui-logger/0.1.11/tui_logger/)
+//! [Documentation](https://docs.rs/tui-logger/0.1.12/tui_logger/)
 //! 
 //! ## Features
 //! 
 //! - [X] Logger implementation for the `log` crate
-//! - [X] Logger enable/disable detection uses fast, collision free hash table
-//! - [ ] Collision free hash table algorithm to be done. Currently use a big table
+//! - [X] Logger enable/disable detection via hash table (avoid string compare)
 //! - [X] Hot logger code only copies enabled log messages with timestamp into a circular buffer
 //! - [X] Widgets/move_message() retrieve captured log messages from hot circular buffer
-//! - [X] Lost message detection of overwritten logs messages in circular buffer
-//! - [X] Log filtering performed on log-target
-//! - [X] Simple Widgets to view the logs and select Debuglevel of target
+//! - [X] Lost message detection due to circular buffer
+//! - [X] Log filtering performed on log record target
+//! - [X] Simple Widgets to view logs and configure debuglevel per target
 //! - [X] Smart Widget with dynamic event dispatcher for `termion` events (see demo code)
 //! - [X] Logging of enabled logs to file
+//! - [X] Event dispatcher for termion key events for smart/simple widget control
+//! - [ ] Allow configuration of target dependent loglevel specifically for file logging
 //! - [ ] Avoid duplicating of target, module and filename in every log record
+//! - [ ] Simultaneous modification of all targets' display/hot logging loglevel by key command
 //! 
 //! ## Smart Widget
 //! 
 //! Smart widget consists of two widgets. Left is the target selector widget and
-//! on the right side the logging messages view scrolling up.
+//! on the right side the logging messages view scrolling up. The target selector widget
+//! can be hidden/shown during runtime via key command.
 //!
 //! The target selector widget looks like this:
 //! 
@@ -46,6 +49,14 @@
 //!   + If any of EWIDT are not shown, then the respective log level is not captured
 //! - Target of the log events can be defined in the log e.g. `warn!(target: "demo", "Log message");`
 //!  
+//! ## Event Dispatcher
+//! 
+//! In order to allow above mentioned control via key events, a dispatcher has been integrated.
+//! The dispatcher as module is independent from the backend, but the widgets are in the moment
+//! specifically only for termion. The event handler queue is dynamically built during drawing of
+//! the tui elements. This allows an easy link between complex ui layouts and the embedded widgets.
+//! This could even be used for mouse events, but this is not yet implemented.
+//! 
 //! ## Smart Widget Key Commands
 //! 
 //! |  KEY   | ACTION 
@@ -207,7 +218,7 @@ impl LevelConfig {
 
 /// These are the sub-structs for the static TUI_LOGGER struct.
 struct HotSelect {
-    hashtable: Vec<(Option<u64>, LevelFilter)>,
+    hashtable: HashMap<u64, LevelFilter>,
     default: LevelFilter,
 }
 struct HotLog {
@@ -285,7 +296,7 @@ impl TuiLogger {
 lazy_static! {
     static ref TUI_LOGGER: TuiLogger = {
         let hs = HotSelect {
-            hashtable: vec![],
+            hashtable: HashMap::with_capacity(1000),
             default: LevelFilter::Info,
         };
         let hl = HotLog {
@@ -309,13 +320,6 @@ lazy_static! {
 
 /// Init the logger and record with `log` crate.
 pub fn init_logger(max_level: LevelFilter) -> Result<(), log::SetLoggerError> {
-    for _ in 0..1027 {
-        TUI_LOGGER
-            .hot_select
-            .lock()
-            .hashtable
-            .push((None, max_level));
-    }
     log::set_max_level(max_level);
     log::set_logger(&*TUI_LOGGER)
 }
@@ -355,16 +359,13 @@ pub fn set_level_for_target(target: &str, levelfilter: LevelFilter) {
     let h = fxhash::hash64(&target);
     TUI_LOGGER.inner.lock().targets.set(target, levelfilter);
     let mut hs = TUI_LOGGER.hot_select.lock();
-    let hl = hs.hashtable.len() as u64;
-    hs.hashtable[(h % hl) as usize] = (Some(h), levelfilter);
+    hs.hashtable.insert(h, levelfilter);
 }
 impl Log for TuiLogger {
     fn enabled(&self, metadata: &Metadata) -> bool {
         let h = fxhash::hash64(metadata.target());
         let hs = self.hot_select.lock();
-        let hl = hs.hashtable.len() as u64;
-        let (opt_hash, levelfilter) = hs.hashtable[(h % hl) as usize];
-        if Some(h) == opt_hash {
+        if let Some(&levelfilter) = hs.hashtable.get(&h) {
             metadata.level() <= levelfilter
         } else {
             metadata.level() <= hs.default
